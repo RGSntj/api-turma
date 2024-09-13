a45ee592c96008683b4cddef52962bdc41c2aab5
Rodrigo Gomes Silva <ra43258559805@cursos.insf>
2024-09-13 11:54:50 -0300

    atualizar turma

--- src/controller/turmaController.js
+++ src/controller/turmaController.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import {
+  atualizarTurma,
   consultarTurmaPeloAno,
+  consultarTurmas,
   inserirTurma,
 } from "../repository/turmaRepository.js";
 
@@ -22,6 +24,18 @@ endpoints.post("/turma", async (req, resp) => {
   }
 });
 
+endpoints.get("/turma", async (req, resp) => {
+  try {
+    const registros = await consultarTurmas();
+
+    return resp.send(registros);
+  } catch (error) {
+    return resp.status(400).send({
+      err: error.message,
+    });
+  }
+});
+
 endpoints.get("/turma/busca", async (req, resp) => {
   const ano = req.query.ano;
 
@@ -29,6 +43,19 @@ endpoints.get("/turma/busca", async (req, resp) => {
     const registros = await consultarTurmaPeloAno(ano);
 
     return resp.send(registros);
+  } catch (error) {
+    return resp.status(400).send({
+      err: error.message,
+    });
+  }
+});
+
+endpoints.put("turma/:id", async (req, resp) => {
+  try {
+    let id = req.params.id;
+    let turma = req.body;
+
+    let linhasafetadas = await atualizarTurma(id, turma);
   } catch (error) {
     return resp.status(400).send({
       err: error.message,
--- src/repository/turmaRepository.js
+++ src/repository/turmaRepository.js
@@ -15,6 +15,36 @@ export async function inserirTurma(turma) {
   return resposta[0].insertId;
 }
 
+export async function atualizarTurma(turma, id) {
+  const comando = `UPDATE tb_turma 
+                    SET nm_turma = ?,
+                        ds_curso = ?,
+                        nr_ano_letivo = ?,
+                        qtd_capacidade = ?,
+                        bt_ativo = ?,
+                        dt_inclusao = ?
+                    WHERE id_turma = ?`;
+
+  const registros = await db.query(comando, [
+    turma.turma,
+    turma.curso,
+    turma.ano,
+    turma.capacidade,
+    turma.ativo,
+    id,
+  ]);
+
+  return registros[0].affectedRows;
+}
+
+export async function consultarTurmas() {
+  const comando = `SELECT *
+                      FROM tb_turma`;
+
+  const registros = await db.query(comando);
+  return registros[0];
+}
+
 export async function consultarTurmaPeloAno(ano) {
   const comando = `SELECT *
                       FROM tb_turma
